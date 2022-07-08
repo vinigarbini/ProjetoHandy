b0b428cf429f585b4d09cbcf6517aa0606627af3
Vinicius Garbini <garbini8116@usp.br>
2022-07-08 16:31:31 -0300

    Correcoes basicas II

--- MaoRobo/MaoRobo.ino
+++ MaoRobo/MaoRobo.ino
@@ -19,14 +19,11 @@ int Dedo3Ang = 0;
 int Dedo4Ang = 0;
 int Dedo5Ang = 0;
 
-// Pinos para a comunicacao serial do bluetooth (RX e TX)
-SoftwareSerial Bluetooth(0,1);
+// Pinos para a comunicacao serial do bluetooth (TX e RX)
+SoftwareSerial Bluetooth(3, 2);
 
-// Variavel do caractere de paridade
-byte Paridade;  
-
-// Teste recebido
-int teste;
+// Caractere de paridade
+byte Paridade;
 
 void setup()
 {
@@ -37,15 +34,15 @@ void setup()
   Bluetooth.begin(38400);
   
   // Setando os pinos dos servomotores
-  Dedo1Servo.attach(5);  
+  Dedo1Servo.attach(4);  
   delay(100); //Delay para setagem de cada servo
-  Dedo2Servo.attach(6);   
+  Dedo2Servo.attach(5);   
   delay(100);
-  Dedo3Servo.attach(7);
+  Dedo3Servo.attach(6);
   delay(100);
-  Dedo4Servo.attach(8);
+  Dedo4Servo.attach(7);
   delay(100);
-  Dedo5Servo.attach(9);
+  Dedo5Servo.attach(8);
   delay(100);
   
   pinMode(Bluetooth, INPUT);
@@ -54,30 +51,6 @@ void setup()
 
 void loop()
 { 
-  //Serial.print("Teste is listening: ");
-  //Serial.println(Bluetooth.isListening());
-  
-  //if (Bluetooth.available()) { // Se existem dados para leitura
-   
-    //Serial.println("Testando");
-  //b1 = bluetooth.read(); //Variável para armazenar o dado atual
-  //command += b1; //Variável para armazenar todos os dados
-   
-  //if (b1 == '\n'){ //Se o dado atual for um pulador de linha (\n)
-    //Serial.print(command); //Printa o comando
-    //command = ""; //Limpa o comando para futuras leituras
-  //}
-  //else{
-  //  Serial.println("sem leitura");
-  //}
-
-  
-  
-  // teste sinal bluetooth recebido
-  //teste = Bluetooth.read();
-  //Serial.print("Sinal Recebido");
-  //Serial.println(Bluetooth.read());  
-
   // Recebimento dos dados via bluetooth
   if(Bluetooth.available()) {
     Paridade = Bluetooth.read();
@@ -86,25 +59,16 @@ void loop()
     Dedo3Ang = Bluetooth.read();    
     Dedo4Ang = Bluetooth.read();    
     Dedo5Ang = Bluetooth.read();       
-    
-   if(Paridade == 'A'){   // Verificando a paridade
-    
-      // Recebimento do sinal dos sensores via bluetooth, entre 0 e 180
-      //if(Dedo1Ang!=255)
+  }
+   
+  if(Paridade == 'A'){   // Verificando a paridade
+      
+      // Acionamento dos motores, baseado nos dados recebidos via bluetooth
       Dedo1Servo.write(Dedo1Ang);
-                                                    
-      //if(Dedo2Ang!=255)
       Dedo2Servo.write(Dedo2Ang);
-
-      //if(Dedo3Ang!=255)
       Dedo3Servo.write(Dedo3Ang);
-
-     //if(Dedo4Ang!=255)
       Dedo4Servo.write(Dedo4Ang);
-
-      //if(Dedo5Ang!=255)
       Dedo5Servo.write(Dedo5Ang);
-    }
- }
+  }
   delay(100);
 }            
